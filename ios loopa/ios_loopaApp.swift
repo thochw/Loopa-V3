b26aaaf23287ac65460d91827fc46e0975e4539b
Thomas Chang <thochw@gmail.com>
2026-01-17 15:32:03 -0500

    Initial Commit

---
+++ ios loopa/ios_loopaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_loopaApp.swift
+//  ios loopa
+//
+//  Created by Thomas CHANG-HING-WING on 2026-01-17.
+//
+
+import SwiftUI
+
+@main
+struct ios_loopaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
